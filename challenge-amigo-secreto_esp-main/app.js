24235776e62635e1072b83486c92d1fb1d38bff9
Diana <dianacbohoqez@gmail.com>
2025-03-15 22:23:46 -0500

    funcion amigos agregar y actualizar

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -3,7 +3,15 @@
 
     function agregarAmigo(){
         let nombreamigo = document.getElementById("amigo");
+        console.log(nombreamigo.value);
        
-       console.log(nombreamigo.value);
-       
+        if(nombreamigo.value.trim() === ""){
+            alert("Por favor, inserte un nombre.");
+        }else{
+            amigos.push(nombreamigo.value);
+
+            console.log(`Amigo añadido: ${nombreamigo.value}`);
+            console.log(`Lista actual de amigos: ${amigos}`);
+            nombreamigo.value = "";
+        }
     }
