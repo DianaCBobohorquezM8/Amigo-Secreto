b64323d7b1764640d28fc9c042822d7d20b07fb4
Diana <dianacbohoqez@gmail.com>
2025-03-16 20:01:14 -0500

    creando funcion mostrar lista amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -12,6 +12,31 @@
 
             console.log(`Amigo añadido: ${nombreamigo.value}`);
             console.log(`Lista actual de amigos: ${amigos}`);
+
+            mostrarListaAmigos();
             nombreamigo.value = "";
         }
     }
+
+   
+    function mostrarListaAmigos() {
+    let listaAmigos = document.getElementById("amigos");
+    listaAmigos.innerHTML = "";
+
+        for( let i =0; i< amigos.length; i++){
+        let elementoLi = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(item);
+        }
+    }
+
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
